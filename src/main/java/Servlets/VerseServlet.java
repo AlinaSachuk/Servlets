a31dd6da3fa45c630de324895c37b6da58c01fe9
AlinaSachuk <alinko-91@yandex.ru>
2022-12-23 21:51:54 +0300

    homework 24 complete

--- src/main/java/Servlets/VerseServlet.java
+++ src/main/java/Servlets/VerseServlet.java
@@ -1,11 +1,12 @@
 package Servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet("/verse")
 public class VerseServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
